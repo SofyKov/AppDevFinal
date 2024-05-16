dad15006b0933864793779d84ec015cf198d2055
Grechelle Uy <grechelleuy@yahoo.com>
2024-05-16 11:46:52 -0400

    IT WORKSSS

--- AppDev-Final-Project/Navigation/Navigation/NavigationForm.Designer.cs
+++ AppDev-Final-Project/Navigation/Navigation/NavigationForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.navHomeLabel = new System.Windows.Forms.Label();
             this.aboutUsLabel = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.goToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -43,7 +44,6 @@
             this.tableAdapterManager = new Navigation.MontreWildDBDataSetTableAdapters.TableAdapterManager();
             this.plantTableAdapter = new Navigation.MontreWildDBDataSetTableAdapters.PlantTableAdapter();
             this.plantBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.navHomeLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.montreWildDBDataSet)).BeginInit();
@@ -58,19 +58,34 @@
             this.panel1.Controls.Add(this.aboutUsLabel);
             this.panel1.Controls.Add(this.menuStrip1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1042, 67);
+            this.panel1.Size = new System.Drawing.Size(2431, 149);
             this.panel1.TabIndex = 11;
+            // 
+            // navHomeLabel
+            // 
+            this.navHomeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.navHomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.navHomeLabel.Image = global::Navigation.Properties.Resources.smallLogo;
+            this.navHomeLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.navHomeLabel.Location = new System.Drawing.Point(33, 40);
+            this.navHomeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.navHomeLabel.MaximumSize = new System.Drawing.Size(481, 80);
+            this.navHomeLabel.Name = "navHomeLabel";
+            this.navHomeLabel.Size = new System.Drawing.Size(481, 80);
+            this.navHomeLabel.TabIndex = 9;
+            this.navHomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.navHomeLabel.Click += new System.EventHandler(this.navHomeLabel_Click);
             // 
             // aboutUsLabel
             // 
             this.aboutUsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.aboutUsLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aboutUsLabel.Location = new System.Drawing.Point(881, 18);
-            this.aboutUsLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.aboutUsLabel.Location = new System.Drawing.Point(2056, 40);
+            this.aboutUsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.aboutUsLabel.Name = "aboutUsLabel";
-            this.aboutUsLabel.Size = new System.Drawing.Size(123, 36);
+            this.aboutUsLabel.Size = new System.Drawing.Size(287, 80);
             this.aboutUsLabel.TabIndex = 8;
             this.aboutUsLabel.Text = "ABOUT US";
             this.aboutUsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -81,13 +96,14 @@
             this.menuStrip1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.goToToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(243, 18);
+            this.menuStrip1.Location = new System.Drawing.Point(567, 40);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(13, 4, 13, 4);
-            this.menuStrip1.Size = new System.Drawing.Size(97, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(30, 9, 30, 9);
+            this.menuStrip1.Size = new System.Drawing.Size(212, 70);
             this.menuStrip1.TabIndex = 13;
             this.menuStrip1.Text = "navMenuStrip";
             // 
@@ -102,14 +118,14 @@
             this.goToToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 12F);
             this.goToToolStripMenuItem.ImageTransparentColor = System.Drawing.SystemColors.ActiveCaption;
             this.goToToolStripMenuItem.Name = "goToToolStripMenuItem";
-            this.goToToolStripMenuItem.Size = new System.Drawing.Size(69, 25);
+            this.goToToolStripMenuItem.Size = new System.Drawing.Size(150, 52);
             this.goToToolStripMenuItem.Text = "GO TO";
             // 
             // animalsToolStripMenuItem
             // 
             this.animalsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.animalsToolStripMenuItem.Name = "animalsToolStripMenuItem";
-            this.animalsToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.animalsToolStripMenuItem.Size = new System.Drawing.Size(387, 56);
             this.animalsToolStripMenuItem.Text = "Animals";
             this.animalsToolStripMenuItem.Click += new System.EventHandler(this.animalsToolStripMenuItem_Click);
             // 
@@ -117,7 +133,7 @@
             // 
             this.plantsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.plantsToolStripMenuItem.Name = "plantsToolStripMenuItem";
-            this.plantsToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.plantsToolStripMenuItem.Size = new System.Drawing.Size(387, 56);
             this.plantsToolStripMenuItem.Text = "Plants";
             this.plantsToolStripMenuItem.Click += new System.EventHandler(this.plantsToolStripMenuItem_Click);
             // 
@@ -125,7 +141,7 @@
             // 
             this.encyclopediaToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.encyclopediaToolStripMenuItem.Name = "encyclopediaToolStripMenuItem";
-            this.encyclopediaToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.encyclopediaToolStripMenuItem.Size = new System.Drawing.Size(387, 56);
             this.encyclopediaToolStripMenuItem.Text = "Encyclopedia";
             this.encyclopediaToolStripMenuItem.Click += new System.EventHandler(this.encyclopediaToolStripMenuItem_Click);
             // 
@@ -133,7 +149,7 @@
             // 
             this.gamesToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.gamesToolStripMenuItem.Name = "gamesToolStripMenuItem";
-            this.gamesToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.gamesToolStripMenuItem.Size = new System.Drawing.Size(387, 56);
             this.gamesToolStripMenuItem.Text = "Games";
             this.gamesToolStripMenuItem.Click += new System.EventHandler(this.gamesToolStripMenuItem_Click);
             // 
@@ -168,32 +184,17 @@
             this.plantBindingSource.DataMember = "Plant";
             this.plantBindingSource.DataSource = this.montreWildDBDataSet;
             // 
-            // navHomeLabel
-            // 
-            this.navHomeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.navHomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.navHomeLabel.Image = global::Navigation.Properties.Resources.smallLogo;
-            this.navHomeLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.navHomeLabel.Location = new System.Drawing.Point(14, 18);
-            this.navHomeLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.navHomeLabel.MaximumSize = new System.Drawing.Size(206, 36);
-            this.navHomeLabel.Name = "navHomeLabel";
-            this.navHomeLabel.Size = new System.Drawing.Size(206, 36);
-            this.navHomeLabel.TabIndex = 9;
-            this.navHomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.navHomeLabel.Click += new System.EventHandler(this.navHomeLabel_Click);
-            // 
             // NavigationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1042, 476);
+            this.ClientSize = new System.Drawing.Size(2431, 1062);
             this.Controls.Add(this.panel1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "NavigationForm";
-            this.Text = "Form1";
+            this.Text = "MontreWild";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
